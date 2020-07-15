7594242169a9820ebe9d215b5abe36a3396de038
CarlosCastillo10 <carloscastillo090916@gmail.com>
2020-07-14 19:03:57 -0500

    Optimizando algoritmos para almacenar archivos en couchDB

--- persistence.py
+++ persistence.py
@@ -31,7 +31,7 @@ class Persistence:
             db.save(self.course_report)
             
             # Convertir a bytes el contenido del archivo
-            file_index = bytes(open('index.html').read(), 'utf-8')
+            file_index = bytes(open(file_adress + '/index.html').read(), 'utf-8')
             
             # Guardar archivo en couchDB
             db.put_attachment(self.course_report, file_index, filename="index.html")
@@ -41,20 +41,21 @@ class Persistence:
         # Si se esta ejecutando couchDB o en el puerto
         except ConnectionRefusedError:
             print('\033[91m\nError de conexión (Compruebe que haya iniciado couchDB, o que '
-                'el valor del atributo <port> sea el correcto)\n')
+                'el valor del atributo <port> sea el correcto)')
         
         # Si el usuario o la contraseña son incorrectos
         except couchdb.http.Unauthorized:
-            print('\033[91m\nError de autenticación (<username> o <password> incorrecto)\n')
+            print('\033[91m\nError de autenticación (<username> o <password> incorrecto)')
         
         # Si la dirección IP del localhost no es correcta
         except OSError:
-            print('\033[91m\nError de direccion IP (Compruebe la dirección IP del localhost)\n')
+            print('\033[91m\nError de direccion IP (Compruebe la dirección IP del localhost)')
         
         # Cuando no se encuentra la base de datos creada o el nombre es incorrecto
         except couchdb.http.ResourceNotFound:
             print('\033[91m\nRecurso no encontrado (Compruebe si el nombre de la base de datos es '
-                'correcto, o si la base de datos existe)\n')
+                'correcto, o si la base de datos existe)')
         # Cuando el valor del atributo <database_name> del archivo config.yml esta vacio
         except TypeError:
-            print('\033[91m\nError: Valor incorrecto (Debe ingresar el nombre de la base de datos en el atributo <database_name>)\n')
+            print('\033[91m\nError: Valor incorrecto (Debe ingresar el nombre de la base \
+                de datos en el atributo <database_name>)')
